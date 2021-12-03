2eb10f724cbdab85e56ae562565eb80f17fad84b
Rhys Wells <rhyslwells@outlook.com>
2021-12-03 21:16:45 +0000

    Morse code for input word test 1

---
+++ Code/Python_Code/01.1.1_Blink/Blink2_morse.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+########################################################################
+# Filename    : Blink.py
+# Description : Basic usage of GPIO. Let led blink.
+# auther      : www.freenove.com
+# modification: 2019/12/26
+########################################################################
+from gpiozero import LED
+from time import sleep
+
+
+print ('Program is starting ... ')
+
+led = LED(17)           # define LED pin according to BCM Numbering
+# led = LED("J8:11")     # BOARD Numbering
+'''
+# pins numbering, the following lines are all equivalent
+led = LED("GPIO17")     # BCM
+led = LED("BCM17")      # BCM
+led = LED("BOARD11")    # BOARD
+led = LED("WPI0")       # WiringPi
+led = LED("J8:11")      # BOARD
+'''
+
+#########################################################################
+#Morse code:
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
+
+# Function to encrypt the string
+def encrypt(message):
+	cipher = ''
+	for letter in message:
+		if letter != ' ':
+
+			# Looks up the dictionary and adds the
+			# correspponding morse code
+			# along with a space to separate
+			# morse codes for different characters
+			cipher += MORSE_CODE_DICT[letter] + ' '
+		else:
+			# 1 space indicates different characters
+			# and 2 indicates different words
+			cipher += ' '
+
+	return cipher
+
+def main():
+	message=input("Enter a word you wish to put into Morse code: ")
+	# message = "GEEKS-FOR-GEEKS"
+	result = encrypt(message.upper())
+    for i in result:
+        if i =="-":
+            led.on()
+            sleep(1.5)
+            led.off()
+        elif i ==".":
+            led.on()
+            sleep(0.5)
+            led.off()
+        elif i ==" ":
+            led.on()
+            sleep(3.5)
+            led.off()
+        
+#########################################################################
+
+while True:
+    main()
+    sleep(7)
+    print("Did you get the word?","\n")
